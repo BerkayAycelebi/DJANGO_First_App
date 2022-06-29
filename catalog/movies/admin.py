ca4da4ad3daa5a655681a5b6a303cbf8f4fc461f
Rıza Berkay AYÇELEBİ <differentiate.function@gmail.com>
2022-06-29 22:07:41 +0300

    Added and customized django admin panel

--- catalog/movies/admin.py
+++ catalog/movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id','name','created_date','isPublihed')
+    list_display_links =('id','name')
+    list_filter = ('created_date',)
+    list_editable = ('isPublihed',)
+    search_fields = ('name','description')
+    list_per_page = 20
+
+admin.site.register(Movie,MovieAdmin)
--- catalog/movies/models.py
+++ catalog/movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name=models.CharField(max_length=100, verbose_name='Film Adı:')
+    description = models.TextField(verbose_name='Açıklama:')
+    image = models.CharField(max_length=50, verbose_name='Resim:')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Eklenme Tarihi:')
+    isPublihed=models.BooleanField(default=True, verbose_name='Yayında mı?')
+
+    def __str__(self):
+        return self.name
+    def get_image_path(self):
+        return '/img/'+self.image
